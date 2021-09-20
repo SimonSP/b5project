4e51cf010aa84567f8320c7ae8e79afbbc0a489a
Simón SP <ssalvo@saltala.com>
2021-09-20 19:37:15 -0300

    fix port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const helmet = require('helmet')
 const cookieParser = require('cookie-parser')
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 5000
 const app = express()
 
 app.use(morgan('dev'))
