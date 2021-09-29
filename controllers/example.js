eea2422e3bf2cac42e57a6d65ca823296055d88f
Simón SP <ssalvo@saltala.com>
2021-09-29 20:51:10 -0300

    solucionado problema de import en rutas

--- controllers/example.js
+++ controllers/example.js
@@ -2,14 +2,13 @@ const exampleService = require('../services/example');
 
 
 async function exampleController(req, res, next) {
-
   //chequear si los parametros recibidos son correctos
   //enviar los status code
   try {
     const response = await exampleService()
-    return res.status(200).send(response);
+    return res.status(200).json({ success: true, data: [] })
   } catch (error) {
-    return res.status().send(error);
+    return next(error)
   }
 
 }
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require(`express`)
 const router = express.Router()
 
-const exampleController = require('../controllers/example');
+const { exampleController } = require('../controllers/example');
 
 
 router.get(`/`, exampleController);
